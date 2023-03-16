bbf5cfd53bc630c3a77f0906b31cadd9f0b2d153
Zdragun <nazar2001770@gmail.com>
2023-03-16 09:00:22 +0200

    created react app and added main packages

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import axios from 'axios';
+import './App.css';
+import { useState,useEffect } from 'react';
+function App() {
+/*   const [data, setData] = useState([]);
+
+  useEffect(() => {
+  const url = 'https://api.wisey.app/api/v1/core/preview-courses';
+const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJiNzZmNzhiNS02ODk5LTRmYjEtYTBiNy0wM2UyNDlmZDAwM2YiLCJwbGF0Zm9ybSI6InN1YnNjcmlwdGlvbnMiLCJpYXQiOjE2Nzg4OTIwMjQsImV4cCI6MTY3OTc5MjAyNH0.6h8998my4DkJVfc76HUsnE_h5HMfXAZN2BQ5O83yiIw';
+
+axios.get(url, { headers: { Authorization: `Bearer ${token}` } })
+  .then(response => {
+    console.log(response.data);
+  })
+  .catch(error => {
+    console.error(error);
+  });
+  }, []) */
+
+  return (
+
+    <div className="App">
+        <div>
+         dsds
+        </div>
+      
+    </div>
+  );
+}
+
+export default App;
