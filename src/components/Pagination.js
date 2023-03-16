b4e5399bdd0fffd81bdc01c4c862fc467612669f
Zdragun <nazar2001770@gmail.com>
2023-03-16 20:55:22 +0200

    Added Pagination

---
+++ src/components/Pagination.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const Pagination = ({totalPosts,counItemPage,setCurrentPage}) => {
+    let pages = [];
+    for (let i = 1; i<=Math.ceil(totalPosts/counItemPage); i++){
+        pages.push(i)
+       
+    }
+  return (
+    <div className='pagination'>
+        {
+            pages.map((page, index) => {
+                return (
+                    <button key={index} onClick={() => setCurrentPage(page)}>
+                        {page}
+                    </button>
+                );
+            })
+        }
+    </div>
+  )
+}
+
+export default Pagination
--- src/components/coursesPage.js
+++ src/components/coursesPage.js
@@ -1,17 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../scss/coursepage.scss'
 import Fetcher from '../helpers/Fetcher';
 import ItemList from './ItemList';
+import Pagination from './Pagination';
 
 const CoursesPage = () => {
   const { data, loading, erorr } = Fetcher("https://api.wisey.app/api/v1/core/preview-courses")
+  const [currentPage, setCurrentPage] = useState(1);
+  const [counItemPage, setCounItemPage] = useState(10)
+
+  const lastIndex = currentPage * counItemPage;
+  const firstIndex = lastIndex - counItemPage;
+  const currentPosts = data.slice(firstIndex, lastIndex)
   return (
     <div>
-      <ItemList 
-      loading={loading}
-      data={data}
-      erorr={erorr}
+      <ItemList
+        loading={loading}
+        data={currentPosts}
+        erorr={erorr}
       />
+      <Pagination
+        totalPosts={data.length}
+        counItemPage={counItemPage} 
+        setCurrentPage={setCurrentPage}
+        />
+        
     </div>
   )
 }
